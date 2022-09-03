20807641f043d5c11c2b4f98be2181043df1ed5e
Pomogadryg <denver1033@gmail.com>
2022-09-03 15:46:22 +0300

    Update sudoku_solver_draft.py

--- sudoku_solver_draft.py
+++ sudoku_solver_draft.py
@@ -9,17 +9,17 @@ def counter(lst):
 	return(ctr)
 
 matrix_nominal=[
-['','','',  '4','9','',  '2','',''],
-['','','6',  '','1','',  '','4','7'],
-['','','4' , '','5','',  '','9',''],
+['','8','1',  '2','','4',  '','',''],
+['6','','',  '','','',  '3','',''],
+['','','' , '','1','6',  '','','2'],
 
-['6','7','',  '5','','1',  '4','',''],
-['8','','',  '','4','',  '7','',''],
-['5','4','9',  '8','','',  '3','','6'],
+['','3','6',  '','7','',  '2','',''],
+['1','5','',  '','','',  '7','','8'],
+['','','',  '4','','',  '','','1'],
 
-['4','1','',  '9','6','',  '8','',''],
-['','','',  '1','','5',  '9','','4'],
-['','2','8',  '','7','',  '','6','5']]
+['','','',  '8','','',  '','5','3'],
+['8','9','3',  '','5','1',  '','2','7'],
+['2','1','',  '3','4','7',  '','6','9']]
 
 matrix=matrix_nominal
 
@@ -68,6 +68,7 @@ def solver(pos_x,pos_y,matrix):
 		for idx_x_for_rev_matrix in range(9):
 			list_rev.append(matrix[idx_x_for_rev_matrix][pos_y])
 
+
 		list_coube=[] # new list for cube matrix k_v is a templ
 		for j in k_v:
 			if int_to_str in j:
@@ -78,31 +79,63 @@ def solver(pos_x,pos_y,matrix):
 		if rnd_num_to_test not in matrix[pos_x] and rnd_num_to_test not in list_rev and rnd_num_to_test not in list_coube :
 
 			matrix[pos_x][pos_y]=rnd_num_to_test
-			
+	# print("rev_list: ",list_rev)
+
 		
 
+def findWhereToStart(matrix_int_2):
+	list_of_sum_horiz=[]
+	max_temp=0
+	max_coord=0
+	for i in matrix_int_2:
+		temp_int_2=0
+		for j in i:
+			if j!='':
+				temp_int_2+=1
+		list_of_sum_horiz.append(temp_int_2)
+	for idx_ii,ii in enumerate(list_of_sum_horiz):
+		if ii>max_temp and ii!=9:
+			max_temp=ii
+			max_coord=idx_ii
+
+
+	return(max_coord)
 
 
 	
-def start(matrix):
+def start(matrix_int_1):
 	
-	for idx_i,i in enumerate(matrix): # foa we convert all the element of matrix into int
+	
+	for idx_i,i in enumerate(matrix_int_1): #  we convert all the element of matrix_int_1 into int
 		for idx_p,p in enumerate(i):
 			if p!='':
 				p=int(p)
-				matrix[idx_i][idx_p]=p
-
-	for idx_l,l in enumerate(matrix): # looking up for the first '' element and passing it into def solver.8
-		for idx_elem,elem in enumerate(l):
-			# print(l)
+				matrix_int_1[idx_i][idx_p]=p
+	for counter_int_1 in range(100):
+		where_to_start_idx=findWhereToStart(matrix_int_1)
+		for idx_elem, elem in enumerate(matrix_int_1[where_to_start_idx]):
 			if elem=='':
-				# print(idx_l,idx_elem)
-				solver((idx_l),(idx_elem),matrix)
+				# print(where_to_start_idx,idx_elem,matrix_int_1)
+				solver(where_to_start_idx,idx_elem,matrix_int_1)
+
+
+
+
+	# for idx_l,l in enumerate(matrix_int_1): # looking up for the first '' element and passing it into def solver.8
+	# 	for idx_elem,elem in enumerate(l):
+	# 		# print(l)
+	# 		if elem=='':
+				# print(idx_l,idx_elem, matrix_int_1)
+				# solver((idx_l),(idx_elem),matrix_int_1)
 				# pass
-	print(counter(matrix))
+
 
 
 start(matrix)
+
+for ii in matrix:
+	print(ii)
+
 
 # count=0
 # for m in matrix:
